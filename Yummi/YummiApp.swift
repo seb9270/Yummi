90f7536a8cdbab5ba45a2b40703143d5bacdccd9
Sebastian Micko <Micko.S@etoncollege.org.uk>
2024-01-23 13:00:50 +0000

    Initial Commit

---
+++ Yummi/YummiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YummiApp.swift
+//  Yummi
+//
+//  Created by Micko, Sebastian (AMM) on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct YummiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
